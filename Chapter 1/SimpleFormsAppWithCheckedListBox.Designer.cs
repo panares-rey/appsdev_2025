2b438d3759b9ff48c2163fa846e4eeea6f9fb5d8
Pañares_Rey_Lendon <panaresreylendong@gmail.com>
2025-03-12 10:08:46 +0800

    Add files via upload

---
+++ Chapter 1/SimpleFormsAppWithCheckedListBox.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace CheckListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FruitCheckBoxList = new System.Windows.Forms.CheckedListBox();
+            this.fruitListBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My Simple Fruit Allergy App";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(106, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fruits List";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(352, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "My Fruits List";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // FruitCheckBoxList
+            // 
+            this.FruitCheckBoxList.BackColor = System.Drawing.SystemColors.Info;
+            this.FruitCheckBoxList.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FruitCheckBoxList.FormattingEnabled = true;
+            this.FruitCheckBoxList.Location = new System.Drawing.Point(106, 108);
+            this.FruitCheckBoxList.Name = "FruitCheckBoxList";
+            this.FruitCheckBoxList.Size = new System.Drawing.Size(240, 220);
+            this.FruitCheckBoxList.TabIndex = 3;
+            this.FruitCheckBoxList.SelectedIndexChanged += new System.EventHandler(this.FruitCheckBoxList_SelectedIndexChanged);
+            // 
+            // fruitListBox
+            // 
+            this.fruitListBox.BackColor = System.Drawing.SystemColors.Info;
+            this.fruitListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fruitListBox.FormattingEnabled = true;
+            this.fruitListBox.ItemHeight = 24;
+            this.fruitListBox.Location = new System.Drawing.Point(352, 108);
+            this.fruitListBox.Name = "fruitListBox";
+            this.fruitListBox.Size = new System.Drawing.Size(259, 220);
+            this.fruitListBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleGreen;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(106, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCoral;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(617, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 44);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Bisque;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(617, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 41);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "CLEAR";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fruitListBox);
+            this.Controls.Add(this.FruitCheckBoxList);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SimpleFormsAppWithCheckedListBox ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox FruitCheckBoxList;
+        private System.Windows.Forms.ListBox fruitListBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Chapter 1/SimpleFormsAppWithCheckedListBox.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] fruits = { "Home Alone", "Venom", "Rick and Morty");
+            FruitCheckBoxList.CheckOnClick = true;
+
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var fruit in FruitCheckBoxList.Items)
+            {
+                bool isSelected = FruitCheckBoxList.GetItemChecked(FruitCheckBoxList.Items.IndexOf(fruit));
+                if (isSelected && !fruitListBox.Items.Contains(fruit))
+                {
+                    fruitListBox.Items.Add(fruit); 
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (fruitListBox.SelectedItem != null)
+            {
+                
+                fruitListBox.Items.Remove(fruitListBox.SelectedItem);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            fruitListBox.Items.Clear();
+        }
+
+        private void FruitCheckBoxList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
