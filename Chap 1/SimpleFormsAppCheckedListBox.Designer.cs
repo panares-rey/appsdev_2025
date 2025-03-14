32a00ffca33ca5a5b9e9d1a215f9c969c683fdcc
Pañares_Rey_Lendon <panaresreylendong@gmail.com>
2025-03-14 11:21:50 +0800

    Add files via upload

---
+++ Chap 1/SimpleFormsAppCheckedListBox.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MoviecheckedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.MovielistBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MoviecheckedListBox1
+            // 
+            this.MoviecheckedListBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.MoviecheckedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MoviecheckedListBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MoviecheckedListBox1.FormattingEnabled = true;
+            this.MoviecheckedListBox1.Location = new System.Drawing.Point(92, 107);
+            this.MoviecheckedListBox1.Name = "MoviecheckedListBox1";
+            this.MoviecheckedListBox1.Size = new System.Drawing.Size(209, 244);
+            this.MoviecheckedListBox1.TabIndex = 0;
+            // 
+            // MovielistBox1
+            // 
+            this.MovielistBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.MovielistBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MovielistBox1.FormattingEnabled = true;
+            this.MovielistBox1.ItemHeight = 20;
+            this.MovielistBox1.Location = new System.Drawing.Point(329, 107);
+            this.MovielistBox1.Name = "MovielistBox1";
+            this.MovielistBox1.Size = new System.Drawing.Size(201, 244);
+            this.MovielistBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(187, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(374, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Simple Favorate Movie Picker App";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(92, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Movie/Series List";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(325, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Movie List";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(92, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 41);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightYellow;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(536, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 47);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "REMOVE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(536, 285);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 48);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "CLEAR";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MovielistBox1);
+            this.Controls.Add(this.MoviecheckedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox MoviecheckedListBox1;
+        private System.Windows.Forms.ListBox MovielistBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Chap 1/SimpleFormsAppCheckedListBox.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] movies = { "Rick and Morty", "End Game", "Venom", "Home Alone" };
+            MoviecheckedListBox1.Items.AddRange(movies);    
+            MoviecheckedListBox1.CheckOnClick = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var movies in MoviecheckedListBox1.Items)
+            {
+                bool isSelected = MoviecheckedListBox1.GetItemChecked(MoviecheckedListBox1.Items.IndexOf(movies));
+                if (isSelected && !MovielistBox1.Items.Contains(movies))
+                {
+                    MovielistBox1.Items.Add(movies);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MovielistBox1.Items.Count >= 1)
+            {
+                MovielistBox1.Items.Remove(MovielistBox1.SelectedItem);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MovielistBox1.Items.Clear();
+        }
+    }
+}
